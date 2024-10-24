693ac105f5d6feeda8aa7ff82e77839181c42fa3
Mauri <mauri.monzon91@gmail.com>
2024-10-24 10:51:15 -0300

    Codigo de estado

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ async function bootstrap() {
   //Validaciones
   app.useGlobalPipes(
     new ValidationPipe({
-      // Todos los campos extras que se envien no se toman en cuenta
+      // Todos los campos extras que se reciba por request y no los este esperando no se toman en cuenta
       whitelist: true,
     }),
   );
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
