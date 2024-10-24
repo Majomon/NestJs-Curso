56325657c11719ec452ff71d8aa08bb5175b1e6c
Mauri <mauri.monzon91@gmail.com>
2024-10-24 11:34:10 -0300

    Comando nest g resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TasksSModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { HelloModule } from './hello/hello.module';
+import { PaymentsModule } from './payments/payments.module';
 
 
 @Module({
-  imports: [TasksSModule, UsersModule, HelloModule],
+  imports: [TasksSModule, UsersModule, HelloModule, PaymentsModule],
 })
 
 export class AppModule {}
